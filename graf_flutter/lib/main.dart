08880b32ca6a933cd2c0546fac6cbd2fe3693dd8
Kevin Moore <kevmoo@google.com>
2025-04-30 14:05:44 -0700

    demo graf!

--- graf_flutter/lib/main.dart
+++ graf_flutter/lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:graf/graf.dart';
 
 import 'graf_flutter.dart';
+import 'src/demo_graph.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-final _data = GraphData.randomRing(count: 63);
+final _data = DemoGraph();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,6 +15,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
     theme: ThemeData(scaffoldBackgroundColor: Colors.white),
-    home: Scaffold(body: ForceDirectedGraphView<String>(graphData: _data)),
+    home: Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: ForceDirectedGraphView<int>(graphData: _data)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: _data.churn,
+                icon: const Icon(Icons.refresh),
+              ),
+              IconButton(onPressed: _data.addNode, icon: const Icon(Icons.add)),
+              IconButton(
+                onPressed: _data.removeNode,
+                icon: const Icon(Icons.remove),
+              ),
+              IconButton(
+                onPressed: () {
+                  //_data.addEdge();
+                },
+                icon: const Icon(Icons.link),
+              ),
+              IconButton(
+                onPressed: () {
+                  //_data.removeEdge();
+                },
+                icon: const Icon(Icons.link_off),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
   );
 }
---
+++ graf_flutter/lib/src/demo_graph.dart
@@ -0,0 +1,228 @@
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'dart:collection';
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:graf/graf.dart';
+
+enum DemoGraphDeltaOption { addNode, removeNode, addEdge, removeEdge }
+
+final _rnd = Random();
+
+const _maxDelta = 3;
+const _maxTries = 100;
+
+class DemoGraph implements GraphData<int>, Listenable {
+  DemoGraph({int targetCount = 20}) {
+    this.targetCount = targetCount;
+    for (var i = 0; i < targetCount; i++) {
+      _map[i] = HashSet<int>.identity();
+    }
+
+    for (var i = 0; i < (targetCount - 1); i++) {
+      var added = false;
+      do {
+        final a = _rnd.nextInt(targetCount);
+        final b = _rnd.nextInt(targetCount);
+        added = a != b && !_map[b]!.contains(a) && _map[a]!.add(b);
+      } while (!added);
+    }
+
+    assert(edges.length == (targetCount - 1));
+  }
+
+  final Map<int, Set<int>> _map = HashMap.identity();
+
+  late int _targetCount;
+  int get targetCount => _targetCount;
+  set targetCount(int value) {
+    if (value < 0) {
+      throw ArgumentError.value(value, 'value', 'Cannot be negative');
+    }
+    _targetCount = value;
+  }
+
+  final _notifier = _MyNotifier();
+
+  DemoGraphDeltaOption churn() {
+    final edgeCount = edges.length;
+    final edgeDelta = (_targetCount - 1) - edgeCount;
+    final nodeDelta = _targetCount - _map.length;
+
+    final churnOptions = <DemoGraphDeltaOption>[];
+
+    if (nodeDelta > -_maxDelta) {
+      // we can add nodes – but no more than _maxDelta
+      churnOptions.addAll(
+        Iterable.generate(
+          min(max(nodeDelta, 1), _maxDelta),
+          (i) => DemoGraphDeltaOption.addNode,
+        ),
+      );
+    }
+
+    if (edgeDelta > -_maxDelta &&
+        _map.length > 1 &&
+        edgeCount < (_targetCount - 1)) {
+      // we can add edges – but no more than _maxDelta
+      churnOptions.addAll(
+        Iterable.generate(
+          min(max(edgeDelta, 1), _maxDelta),
+          (i) => DemoGraphDeltaOption.addEdge,
+        ),
+      );
+    }
+
+    if (nodeDelta < _maxDelta && _map.isNotEmpty) {
+      // we can remove nodes!
+      churnOptions.addAll(
+        Iterable.generate(
+          min(max(-nodeDelta, 1), _maxDelta),
+          (i) => DemoGraphDeltaOption.removeNode,
+        ),
+      );
+    }
+
+    if (edgeDelta < _maxDelta && edgeCount > 0) {
+      // we can remove edges!
+      churnOptions.addAll(
+        Iterable.generate(
+          min(max(-nodeDelta, 1), _maxDelta),
+          (i) => DemoGraphDeltaOption.removeEdge,
+        ),
+      );
+    }
+
+    if (churnOptions.isEmpty) {
+      print('noop for now!');
+      return DemoGraphDeltaOption.removeNode;
+    }
+
+    assert(churnOptions.isNotEmpty);
+
+    final option = churnOptions[_rnd.nextInt(churnOptions.length)];
+
+    print('picked $option from $churnOptions');
+
+    switch (option) {
+      case DemoGraphDeltaOption.addNode:
+        addNode();
+      case DemoGraphDeltaOption.removeNode:
+        removeNode();
+      case DemoGraphDeltaOption.addEdge:
+        addEdge();
+      case DemoGraphDeltaOption.removeEdge:
+        removeEdge();
+    }
+    return option;
+  }
+
+  void addNode() {
+    // TODO: we could be more efficient by tracking the nodes we've randomly removed
+    var newNode = -1;
+    for (var i = 0; i < (_map.length + 1); i++) {
+      if (!_map.containsKey(i)) {
+        newNode = i;
+        break;
+      }
+    }
+    assert(newNode != -1);
+
+    _map[newNode] = HashSet<int>.identity();
+
+    print('added $newNode');
+    _notifier._notify();
+  }
+
+  void removeNode() {
+    if (_map.isEmpty) {
+      throw UnsupportedError('Cannot remove a node from an empty map');
+    }
+
+    final key = _randomNode();
+    _map.remove(key);
+
+    var removeCount = 0;
+    for (var e in _map.values) {
+      if (e.remove(key)) {
+        removeCount++;
+      }
+    }
+
+    print('removed $key with $removeCount edges');
+    _notifier._notify();
+  }
+
+  void addEdge() {
+    if (_map.isEmpty) {
+      throw UnsupportedError('Cannot add an edge to an empty map');
+    }
+    var count = 0;
+    var added = false;
+    do {
+      count++;
+      final a = _randomNode();
+      final b = _randomNode();
+      added = a != b && !_map[b]!.contains(a) && _map[a]!.add(b);
+      if (added) {
+        print('Added edge: $a -> $b');
+      } else if (count > _maxTries) {
+        throw StateError(
+          'could not find a place to add an edge after $_maxTries tries',
+        );
+      }
+    } while (!added);
+  }
+
+  void removeEdge() {
+    if (_map.length < 2) {
+      throw UnsupportedError(
+        'Cannot remove an edge from an map with 0 or 1 nodes',
+      );
+    }
+
+    var count = 0;
+    var removed = false;
+    do {
+      count++;
+      final a = _randomNode();
+      final b = _randomNode();
+      removed = a != b && (_map[a]!.remove(b) || _map[b]!.remove(a));
+      if (removed) {
+        print('removed edge: $a -> $b');
+      } else if (count > _maxTries) {
+        throw StateError('could not an edge to remove after $_maxTries tries');
+      }
+    } while (!removed);
+  }
+
+  int _randomNode() {
+    final index = _rnd.nextInt(_map.length);
+    return _map.keys.elementAt(index);
+  }
+
+  //
+  // GraphData bits
+  //
+  @override
+  Iterable<int> edgesFrom(int node) => _map[node]!;
+
+  @override
+  Iterable<int> get nodes => _map.keys;
+
+  //
+  // Listenable bits
+  //
+  @override
+  void addListener(VoidCallback listener) => _notifier.addListener(listener);
+
+  @override
+  void removeListener(VoidCallback listener) =>
+      _notifier.removeListener(listener);
+}
+
+// Avoiding the lint about calling `notifyListeners` being protected/test only.
+final class _MyNotifier extends ChangeNotifier {
+  void _notify() => notifyListeners();
+}
