a341f0c9007abf17fee1bdbc15d51e8d38b813b1
Kevin Moore <kevmoo@google.com>
2025-05-01 16:32:34 -0700

    cleanup node widget things

--- graf_flutter/lib/src/node_widget.dart
+++ graf_flutter/lib/src/node_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-const _doText = false;
-
 class NodeWidget<T> extends StatelessWidget {
   final T node;
   final double size;
@@ -12,29 +10,43 @@ class NodeWidget<T> extends StatelessWidget {
   Widget build(BuildContext context) => Container(
     width: size,
     height: size,
-    decoration: BoxDecoration(
-      color: Colors.blue.shade100,
-      shape: BoxShape.circle,
-      border: Border.all(color: Colors.blue.shade800, width: 1.5),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.black.withValues(alpha: 0.2),
-          blurRadius: 3,
-          offset: const Offset(1, 1),
-        ),
-      ],
-    ),
+    decoration: _decorationRaw,
     child: Center(
-      child: _doText
-          ? Text(
-              node.toString(),
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: size * 0.2, // Adjust font size relative to node size
-              ),
-              overflow: TextOverflow.ellipsis, // Prevent text overflow
-            )
-          : const Icon(Icons.flag),
+      child: Text(
+        node.toString(),
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: size * 0.2, // Adjust font size relative to node size
+        ),
+        overflow: TextOverflow.ellipsis, // Prevent text overflow
+      ),
     ),
   );
 }
+
+class IconWidget extends StatelessWidget {
+  final double size;
+
+  const IconWidget({super.key, required this.size});
+
+  @override
+  Widget build(BuildContext context) => Container(
+    width: size,
+    height: size,
+    decoration: _decorationRaw,
+    child: const Center(child: Icon(Icons.flag)),
+  );
+}
+
+final _decorationRaw = BoxDecoration(
+  color: Colors.blue.shade100,
+  shape: BoxShape.circle,
+  border: Border.all(color: Colors.blue.shade800, width: 1.5),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black.withValues(alpha: 0.2),
+      blurRadius: 3,
+      offset: const Offset(1, 1),
+    ),
+  ],
+);
